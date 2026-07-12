4a7c3b0f8a1be72642b1c903227b15ef1344697c
Kanishka Garg <kanishkagarg410@gmail.com>
2026-07-12 17:00:22 +0530

    preparing for deployement

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin:true,
+    credentials:true
+}));
 app.use(express.json());
 app.use("/uploads",express.static("uploads"));
 
